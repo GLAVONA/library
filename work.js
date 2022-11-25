646dec2778e40ae250a2e8a8388d74c4f7bc41a6
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2022-11-25 11:38:03 +0200

    .

--- work.js
+++ work.js
@@ -2,37 +2,100 @@ const table = document.getElementById("table");
 const container = document.querySelector("container");
 const form = document.getElementById("book-form");
 const newBookButton = document.getElementById("new-book");
+const submitButton = document.getElementById("submit")
+let bookId = 0;
 
+const myLibrary = [];
 
-let myLibrary = [];
-
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, id){
    this.title = title;
    this.author = author;
    this.pages = pages;
    this.read = read;
-   function info(){
-    return `${title} by ${author}, ${pages}, ${read}`
-   }
+   this.id = id;
 };
 
-function addBookToLibrary(title, author, pages, read){
-   let newBook = Object.create(Book)
-   newBook.title = title;
-   newBook.author = author;
-   newBook.pages = pages;
-   newBook.read = read;
-
-   myLibrary.push(newBook);
+Book.prototype.info = function() {
+   return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
 }
 
+function addBookToLibrary(){
 
-addBookToLibrary("zcxczx","fzxzvx","123","gkmkmgfa");
-addBookToLibrary("mfnkmfa","fzxzvx","123","gkmkmgfa");
-addBookToLibrary("amsfmkla","fzxzvx","123","gkmkmgfa");
-addBookToLibrary("as kdmkma","fzxzvx","123","gkmkmgfa");
+   let titleInput = document.getElementById("title-input").value;
+   let authorInput = document.getElementById("author-input").value;
+   let pagesInput = document.getElementById("pages-input").value;
+   let checkboxInput= document.getElementById("checkbox-input").checked;
+
+   let newBook = Object.create(Book)
+   newBook.title = titleInput;
+   newBook.author = authorInput;
+   newBook.pages = pagesInput;
+   newBook.read = checkboxInput;
+   newBook.id = ++bookId;
+   myLibrary.push(newBook);
+
+   event.preventDefault();
+   if( myLibrary.length===1){
+      createTableHeaders();
+      }
+   renderTable(newBook.title, newBook.author, newBook.pages, newBook.read, bookId);
+   form.classList.remove("enabled")
+}
+
+function renderTable(title, author, pages, read, bookId){
+
+   let tr = document.createElement("tr");
+   tr.setAttribute("id",bookId);
+
+   let td1 = document.createElement("td");
+   td1.textContent = title;
+   tr.appendChild(td1);
+   
+   let td2 = document.createElement("td");
+   td2.textContent = author;
+   tr.appendChild(td2);
+   
+   let td3 = document.createElement("td");
+   td3.textContent = pages;
+   tr.appendChild(td3);
+   
+   let td4 = document.createElement("td");
+   let newCheckBox = document.createElement("input");
+   newCheckBox.type = "checkbox";
+   newCheckBox.checked = read;
+   td4.appendChild(newCheckBox);
+   tr.appendChild(td4);
+   
+   let td5 = document.createElement("td");
+   let btn = document.createElement("input")
+   btn.setAttribute("type","button");
+   btn.setAttribute("value","X");
+   btn.setAttribute("onclick","deleteRow(this, bookId)")
+   td5.appendChild(btn)
+   tr.appendChild(td5);
+
+   table.appendChild(tr);
+}
+
+function deleteRow(row, bookId) {
+   let i = row.parentNode.parentNode.rowIndex;
+   document.getElementById("table").deleteRow(i);
+   removeBookFromArray(myLibrary,bookId-1);
+}
+
+function removeBookFromArray(arr, id) {
+   const objWithIdIndex = arr.findIndex((obj) => obj.id === id);
+ 
+   if (objWithIdIndex > -1) {
+     arr.splice(objWithIdIndex, 1);
+   }
+ 
+   return arr;
+ }
 
 
+function createTableHeaders(){
+   
 const tableHeaders = document.createElement("tr");
 const th1 = document.createElement("th");
 th1.textContent = "Title";
@@ -46,32 +109,19 @@ tableHeaders.appendChild(th3);
 const th4 = document.createElement("th");
 th4.textContent = "Read";
 tableHeaders.appendChild(th4);
-
+const th5 = document.createElement("th");
+th5.textContent = "Delete";
+tableHeaders.appendChild(th5);
 table.appendChild(tableHeaders);
-
-
-for(let book of myLibrary){
-   let tr = document.createElement("tr");
-
-   let td1 = document.createElement("td");
-   td1.textContent = book.title;
-   tr.appendChild(td1);
-   
-   let td2 = document.createElement("td");
-   td2.textContent = book.author;
-   tr.appendChild(td2);
-   
-   let td3 = document.createElement("td");
-   td3.textContent = book.pages;
-   tr.appendChild(td3);
-   
-   let td4 = document.createElement("td");
-   td4.textContent = book.read;
-   tr.appendChild(td4);
-
-   table.appendChild(tr);
 }
 
 newBookButton.addEventListener("click",()=>{
-   form.classList.add("enabled")
+   form.classList.toggle("enabled");
+   form.reset();
 })
+
+submitButton.addEventListener("click",addBookToLibrary);
+
+
+
+
